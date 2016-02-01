470a127df1cf7ceb7d5c82404258a86f061fd39d
Yogesh Patil <ypatil@genius-minds.com>
2016-02-01 18:53:09 +0530

    Test error, REST API.

--- tests/google/googleTest.js
+++ tests/google/googleTest.js
@@ -1,6 +1,8 @@
 module.exports = {
   "Demo test Google" : function (browser) {
+    var sessionID = null;
     browser
+      .session(function(session) { sessionID = session.sessionID })
       .url("http://www.google.com")
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'nightwatch')
@@ -9,5 +11,17 @@ module.exports = {
       .pause(1000)
       .assert.containsText('#main', 'The Night Watch 111')
       .end();
+
+
+    updateStatus(sessionID, "error", "error once again");
   }
+};
+
+function updateStatus(sessionId, newStatus, message) {
+    var baseUrl = "https://yogeshpatil2:1qscJEiXnMdazFwW4ft3@www.browserstack.com";
+    var sessUpdateUrl = baseUrl + "/automate/sessions/" + sessionId + ".json";
+    var newData = "\"{\"status\":\"" + newStatus + "\", \"reason\":\"" + message + "\"}";
+
+    var request = require("request");
+    request({uri: sessUpdateUrl, method:"PUT", form:{"status": newStatus,"reason": message}});
 };
